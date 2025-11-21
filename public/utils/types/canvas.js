28cf6769b280b2c3ec68dd77119705ff89ea84b3
eitancohen77 <eitancohen77@gmail.com>
2025-11-21 09:44:22 -0500

    changed Canvas

--- public/utils/types/canvas.js
+++ public/utils/types/canvas.js
@@ -2,9 +2,6 @@ export function canvas(query, item) {
     
     if (item.backgroundColor) {
         const { r, g, b, a } = item.backgroundColor;
-        query.style.backgroundColor = `rgba(${r*255}, ${g*255}, ${b*255}, ${a})`;
+        document.body.style.backgroundColor = `rgba(${r*255}, ${g*255}, ${b*255}, ${a})`;
     }
-    query.style.position = "relative"
-    query.style.width = "100vw"
-    query.style.height = "100vh"
 }
